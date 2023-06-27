16aec49418099f419a515f30eec3d996a2d6c1e8
Lakshmanan <lakshmanvellingri@gmail.com>
2023-06-27 08:54:18 +0530

    Sample

---
+++ src/app/Sample/sample.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+})
+export class SampleComponent {
+
+  const GENRE = { ROMANTIC: 'romantic', DRAMA: 'drama', COMEDY: 'comedy', DOCUMENTARY: 'documentary', }
+  isEnable:boolean = false;
+
+
+  getReadableStatus(job) {
+    let myName = "Alice";
+    return job.isRunning() ? "Running" : job.hasErrors() ? "Failed" : "Succeeded "; // Noncompliant 
+  }
+
+  enable() {
+    if (this.isEnable == true) {
+      return true;
+    }
+    this.configureFilm(this.GENRE)
+  }
+
+
+  configureFilm(genre) {
+    switch (genre) {
+      case GENRE.ROMANTIC: // some logic to be executed } }
+
+     }
+  }
+
+}
